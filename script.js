904ee8f432227602e39f0080d7fce268cd3b17c6
Natália Evelyn <natalia_evelyn@icloud.com>
2022-07-26 17:08:22 -0300

    Teste Git

---
+++ script.js
@@ -0,0 +1,8 @@
+let num = 5; //Criei uma variável chamada Num e atribui o 5.
+console.log(num)
+console.log(1+1)
+
+let compras = ['banana', 'maça', 'pera']
+console.log(compras)
+alert("Digite Algo")
+compras.push('')
